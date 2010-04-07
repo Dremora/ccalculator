e081fdc84536ff15884829d3bced97a888f84be0
Kirill Korolyov <kirill.korolyov@gmail.com>
2010-04-07 22:49:35 +0200

    Removed not used method

--- src/ccalculator/Value.java
+++ src/ccalculator/Value.java
@@ -1,12 +1,6 @@
 package ccalculator;
 
 public abstract class Value extends Token
-{    
-    protected String findRegex(String where, String regex)
-    {
-         int length = where.length()-where.replaceFirst(regex, "").length();
-         return where.substring(0, length);
-    }
-
+{
     abstract double value();
 }

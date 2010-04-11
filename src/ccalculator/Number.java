08374f05c7be12dd62557d3efca59a3397330fb1
Kirill Korolyov <kirill.korolyov@gmail.com>
2010-04-11 13:46:39 +0200

    Only dot is now recognized as decimal delimiter

--- src/ccalculator/Number.java
+++ src/ccalculator/Number.java
@@ -8,12 +8,12 @@ public class Number extends Value
  
     public Number(String str, boolean sign) throws ParseException
     {
-        Matcher matcher = Pattern.compile(sign ? "\\s*([\\+\\-]?[0-9]+([\\.\\,][0-9]+)?)" : "\\s*([0-9]+([\\.\\,][0-9]+)?)").matcher(str);
+        Matcher matcher = Pattern.compile(sign ? "\\s*([\\+\\-]?[0-9]+(\\.[0-9]+)?)" : "\\s*([0-9]+(\\.[0-9]+)?)").matcher(str);
         if (!matcher.lookingAt())
         {
             throw new ParseException("number", offset);
         }
-        value = Double.parseDouble(matcher.group(1).replace(',', '.'));
+        value = Double.parseDouble(matcher.group(1));
         length = matcher.end();
         offset += length;
     }

909d12920ba87d83c9d30a7dca53edbe206a348a
Kirill Korolyov <kirill.korolyov@gmail.com>
2010-04-12 13:20:32 +0200

    Corrected exception handling in main class

--- src/ccalculator/CCalculator.java
+++ src/ccalculator/CCalculator.java
@@ -15,7 +15,7 @@ public class CCalculator
         String line;
         if (args.length == 0)
         {
-            System.out.println("Calculator v0.1");
+            System.out.println("CCalculator v0.1");
             System.out.println();
             System.out.println("Input a combination of numbers, basic arithmetic operators and brackets, then press <Enter> to get a result");
         
@@ -28,7 +28,17 @@ public class CCalculator
                 {
                     break;
                 }
-                System.out.println(calculate(line));
+                try {
+                	System.out.println(calculate(line));
+                } catch (ParseException e)
+                {
+                    for (int i = -3; i < e.getErrorOffset(); i++)
+                    {
+                        System.err.print(" ");
+                    }
+                    System.err.println("^");
+                    System.err.println(e.getMessage());
+                }
             }
         }
         else
@@ -37,28 +47,24 @@ public class CCalculator
             for (String str: args) {
                 result += str;
             }
-            System.out.println(calculate(result));
+            try {
+				System.out.println(calculate(result));
+			} catch (ParseException e) {
+				System.err.println(e.getErrorOffset() + ": " + e.getMessage());
+			}
         }
     }
     
-    public static String calculate(String line)
+    public static String calculate(String line) throws ParseException
     {
-        try
-        {            
-            // Tokenize
-            Block expression = new Block(line);
-            return Double.toString(expression.value());
+        Block expression = new Block(line);
+        String value = Double.toString(expression.value());
+        if (value.endsWith(".0")) {
+        	value = value.substring(0, value.length() - 2);
+        } else if (value.equals("NaN")) {
+        	value = "Can't calculate result";
         }
-        catch (ParseException e)
-        {
-            System.err.println(e.getMessage());
-            for (int i = -3; i < e.getErrorOffset(); i++)
-            {
-                System.out.print(" ");
-            }
-            System.out.println("^");
-            return e.getMessage();
-        }
+        return value;
     }
     
     public static boolean inArray(char[] array, char value)
